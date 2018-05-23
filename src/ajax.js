e0c1071df257a2bd6ad04ad72c62d1312f8ebd51
Kit Westneat <kit.westneat@gmail.com>
2018-05-23 10:55:57 -0400

    add set_ajax_debug function for intercepting ajax calls

--- src/ajax.js
+++ src/ajax.js
@@ -4,6 +4,11 @@ var utils = require('./utils');
 
 const XHR_DONE = 4;
 
+let ajax_debug = false;
+window.set_ajax_debug = function(cb) {
+    ajax_debug = cb;
+};
+
 /**
  * Simple IE9+ and cross-browser ajax request function
  * Note: x-domain requests in IE9 do not support the use of cookies
@@ -21,6 +26,12 @@ export function ajaxBuilder(timeout = 3000) {
       let x;
       let useXDomainRequest = false;
       let method = options.method || (data ? 'POST' : 'GET');
+
+      if (ajax_debug) {
+          let debug_resp = ajax_debug(url, callback, data, options);
+          if (debug_resp)
+              return;
+      }
 
       let callbacks = typeof callback === 'object' && callback !== null ? callback : {
         success: function() {
