72c998a052670986c8b1dc0e49c6630a3d889a53
Kit Westneat <kit.westneat@gmail.com>
2021-08-11 19:21:03 -0400

    catch bidder errors

--- src/config.js
+++ src/config.js
@@ -499,6 +499,8 @@ export function newConfig() {
     currBidder = bidder;
     try {
       return fn();
+    } catch (e) {
+      console.error("prebid runWithBidder:", bidder, e);
     } finally {
       resetBidder();
     }
